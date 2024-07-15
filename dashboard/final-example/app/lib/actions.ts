bb5cc389138e29efad548c4a82c5dfba12ec1b2f
Robin A.Meade <robin.a.meade@gmail.com>
2024-07-14 17:56:13 -1000

    Combine the create and edit forms into one form component
    The create and edit form components are nearly identical. The differences can be handled by ternary expressions.
    
    Combining the two into one is DRYer.
    
    (The formatting of the nested ternary expressions may
    benefit from the experimental --experimental-ternaries
    option
    https://prettier.io/blog/2023/11/13/curious-ternaries
    )

--- dashboard/final-example/app/lib/actions.ts
+++ dashboard/final-example/app/lib/actions.ts
@@ -110,6 +110,59 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
+export async function createOrUpdateInvoice(
+  id: string | undefined,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: `Missing Fields. Failed to ${id ? 'Update' : 'Create'} Invoice.`,
+      payload: formData,
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+
+  if (id) {
+    // Update existing invoice record in the database
+    try {
+      await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+  } else {
+    // Insert new invoice record into the database
+    const date = new Date().toISOString().split('T')[0];
+    try {
+      await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    } catch (error) {
+      return {
+        message: 'Database Error: Failed to Create Invoice.',
+      };
+    }
+  }
+
+  // Revalidate the cache for the invoices page and redirect the user.
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
 export async function deleteInvoice(id: string) {
   // throw new Error('Failed to Delete Invoice');
 
